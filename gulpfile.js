62a5194d4d4db99d092436b33f02d4b47a4c145d
Klim Lyapin <westn23@mail.ru>
2017-12-15 17:16:10 +0400

    added pug to html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const pug = require('gulp-pug');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/*.sass')
@@ -7,6 +8,13 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
 
+gulp.task('pug', function() {
+  return gulp.src('src/views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('src/dest'))
+});
+
 gulp.task('default', function () {
-  gulp.watch('./sass/**/*.sass', ['sass']);
+  gulp.watch('sass/**/*.sass', ['sass']);
+  gulp.watch('src/views/*.pug', ['pug']);
 });
