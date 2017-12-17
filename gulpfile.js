860efffda53caba353d49e83ee044c6fc018092f
Klim Lyapin <westn23@mail.ru>
2017-12-17 18:36:06 +0400

    added styles, blocks and structurized

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const connect = require('gulp-connect');
 
-gulp.task('sass', function () {
-  return gulp.src('src/stylesheets/*.sass')
+gulp.task('scss', function () {
+  return gulp.src('src/stylesheets/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(connect.reload())
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('pug', function() {
   return gulp.src('src/views/*.pug')
-    .pipe(pug())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(connect.reload())
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', function () {
-  gulp.watch('sass/**/*.sass', ['sass']);
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true,
+    fallback: 'index.html',
+    port: 8000
+  });
+});
+
+gulp.task('build', ['scss', 'pug', 'connect'])
+
+gulp.task('default', ['build'], function () {
+  gulp.watch('src/stylesheets/*.scss', ['scss']);
   gulp.watch('src/views/*.pug', ['pug']);
 });
