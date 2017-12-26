147df2489538d304a5600cf333a816e1edadd0e9
Klim Lyapin <westn23@mail.ru>
2017-12-27 02:55:58 +0400

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 const connect = require('gulp-connect');
+const image = require('gulp-image');
 
 gulp.task('scss', function () {
   return gulp.src('src/stylesheets/*.scss')
@@ -19,18 +20,26 @@ gulp.task('pug', function() {
     .pipe(gulp.dest('dist'))
 });
 
+gulp.task('image', function () {
+  gulp.src('src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('dist/img'));
+});
+
 gulp.task('connect', function() {
   connect.server({
     root: 'dist',
     livereload: true,
-    fallback: 'index.html',
+    fallback: 'dist/index.html',
     port: 8000
   });
 });
 
-gulp.task('build', ['scss', 'pug', 'connect'])
+gulp.task('build', ['scss', 'pug', 'image', 'connect'])
 
 gulp.task('default', ['build'], function () {
   gulp.watch('src/stylesheets/*.scss', ['scss']);
+  gulp.watch('src/stylesheets/**/*.scss', ['scss']);
   gulp.watch('src/views/*.pug', ['pug']);
+  gulp.watch('src/images/*', ['image']);
 });
